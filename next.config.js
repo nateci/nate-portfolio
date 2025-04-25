e9437bb001c0ab6ccccf346f8b8c62eb87cd12c0
Nate Cirino <nathanielcirino@gmail.com>
2025-04-25 11:50:35 -0500

    aaa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com'],
-    formats: ['image/avif', 'image/webp'],
-    imageSizes: [128, 256, 512, 1024, 2048],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        destination: 'https://natecirino.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
+	reactStrictMode: true,
+	images: {
+		domains: ['res.cloudinary.com'],
+		formats: ['image/avif', 'image/webp'],
+		imageSizes: [128, 256, 512, 1024, 2048],
+	},
 };
