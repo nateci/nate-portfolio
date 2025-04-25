4aa6b94449e9c1e8d9520a505e1ce69f3e96d6d9
Nate Cirino <nathanielcirino@gmail.com>
2025-04-25 11:28:38 -0500

    fixing https routing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: ['res.cloudinary.com'],
-		formats: ['image/avif', 'image/webp'],
-		imageSizes: [128, 256, 512, 1024, 2048],
-	},
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+    formats: ['image/avif', 'image/webp'],
+    imageSizes: [128, 256, 512, 1024, 2048],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://natecirino.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
